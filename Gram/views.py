5c7d12db05216162ac11eecb5a1f3a83b916ce16
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-02 18:40:47 +0300

    Profile route working

--- Gram/views.py
+++ Gram/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Image
 # Create your views here.
 
 
+@login_required(login_url='/accounts/login/')
 def home(request):
     test = 'Working'
     image_test = Image.objects.all()
@@ -11,3 +13,12 @@ def home(request):
         "image_test": image_test
     }
     return render(request, 'index.html', content)
+
+
+# @login_required(login_url='/accounts/login/')
+def profile(request):
+    test = 'Profile route Working'
+    content = {
+        "test": test,
+    }
+    return render(request, 'profiles/profile.html', content)
