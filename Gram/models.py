6fe5707601e123e97211dfdde2042de799285339
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-02 12:22:59 +0300

    Image upload working

--- Gram/models.py
+++ Gram/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+# import datetime as dt
 # Create your models here.
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/', blank=True)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def save_images(self):
+        self.save()
--- Gram/views.py
+++ Gram/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-
+from .models import Image
 # Create your views here.
 
 
 def home(request):
     test = 'Working'
+    image_test = Image.objects.all()
     content = {
         "test": test,
+        "image_test": image_test
     }
     return render(request, 'index.html', content)
