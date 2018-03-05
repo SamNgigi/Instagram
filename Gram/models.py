33fd3c085a40fd3a1eddbf8124ab47f2b5f438f9
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-05 07:29:23 +0300

    Added post foreign key to profiles

--- Gram/models.py
+++ Gram/models.py
@@ -17,26 +17,6 @@ class Tag(models.Model):
         self.save()
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    bio = models.CharField(max_length=200, blank=True)
-    profile_pic = models.ImageField(
-        upload_to='profile/', blank=True, null=True)
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
-
 class Image(models.Model):
     image = models.ImageField(upload_to='images/', blank=True, null=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
@@ -50,6 +30,29 @@ class Image(models.Model):
 
     def save_images(self):
         self.save()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    bio = models.CharField(max_length=200, blank=True)
+    profile_pic = models.ImageField(
+        upload_to='profile/', blank=True, null=True)
+    post = models.ForeignKey(
+        Image, on_delete=models.CASCADE,
+        related_name='profile', blank=True, null=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
 class Comment(models.Model):
