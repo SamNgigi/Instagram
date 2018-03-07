ea42f192e77fb87f49c5393a83e513a6e384cab2
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-07 10:20:15 +0300

    like css working well enough

--- static/Js/main.js
+++ static/Js/main.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
-  // console.log('working');
+  console.log('working');
   // alert('Working')
-    $('#heart').on('click', function() {
-      // e.preventDefault()
-      $('.fa-heart').toggle()
+    // $('.fa-heart').hide()
+    $('.fa-heart').on('click', function(e) {
+      e.preventDefault()
+      var postId = $(this).attr('id').split('-')[1];
+      console.log("clicked" + postId);
+      // $(this).('.fa-heart-o, .fa-heart').toggle()
+      $(this).toggleClass("red")
   })
 });
